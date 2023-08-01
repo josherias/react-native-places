51425dfdd5bda7c64b307cc9d561c18fa0f64960
Josh Erias <josherias10@gmail.com>
2023-08-01 09:50:39 +0300

    second commit

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className="bg-red-500 text-white h-[100vh] flex items-center justify-center">
-      <Text className="text-white">
-        Open up App.js to start working on your apps here!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex items-center justify-center">
+      <Text className="text-red-500">HomeScreen is here</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
